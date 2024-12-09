c6dfb4ac9a1dd866f6b300c0eb53476ea03d3050
kirill potemkin <kirill.potemkin@gmail.com>
2024-12-09 09:17:33 +0300

    обновления

---
+++ Levels.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConsole
+{
+    public class Levels
+    {
+        //---------------------------конструктор-------------------------
+        public Levels(decimal praice) 
+        { 
+        
+        }
+        //----------------------------Поля-------------------------------
+        #region Fields
+        /// <summary>
+        /// цена уровня
+        /// </summary>
+        public decimal Praice = 0;
+        /// <summary>
+        /// лот уровня
+        /// </summary>
+        public decimal LotLevel = 0;
+        /// <summary>
+        /// Открытый объём
+        /// </summary>
+        public decimal Volume = 0;
+        #endregion
+    }
+}
